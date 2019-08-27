a20d17ae6289da8414182aab36e8a880beb1f8fe
Stefan Wong <stfnwong@gmail.com>
2019-08-27 14:26:10 +1200

    Updated scripts, added some massif reports

--- scripts/profile_tree.sh
+++ scripts/profile_tree.sh
@@ -5,8 +5,13 @@
 # Ensure that we have binaries
 make clean && make all
 
-TREE_TRAVERSER_FILE=tree_traverser.massif
-valgrind --tool=massif --massif-out-file=$TREE_TRAVERSER_FILE ./bin/tree_traverser
+TREE_TRAVERSER_FILE=tree_traverser
+valgrind --tool=massif --massif-out-file=reports/$TREE_TRAVERSER_FILE.massif ./bin/tree_traverser
 
-LC_TREE_TRAVERSER_FILE=lc_tree_traverser.massif
-valgrind --tool=massif --massif-out-file=$LC_TREE_TRAVERSER_FILE ./bin/lc_tree_traverser
+LC_TREE_TRAVERSER_FILE=lc_tree_traverser
+valgrind --tool=massif --massif-out-file=reports/$LC_TREE_TRAVERSER_FILE.massif ./bin/lc_tree_traverser
+
+# Dump reports to text file 
+ms_print reports/$TREE_TRAVERSER_FILE.massif > reports/$TREE_TRAVERSER_FILE.txt 
+
+ms_print reports/$LC_TREE_TRAVERSER_FILE.massif > reports/$LC_TREE_TRAVERSER_FILE.txt 
