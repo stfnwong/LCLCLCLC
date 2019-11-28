2df19871ff4f45a2db01eb01942f783cf1e93793
Stefan Wong <stfnwong@gmail.com>
2019-11-28 23:41:12 +0000

    Added some python implementations to questions

--- pylc/questions.py
+++ pylc/questions.py
@@ -5,17 +5,55 @@ Answers to specific Leetcode questions
 Stefan Wong 2019
 """
 
-def maximum_subarray_53(nums:list) -> int:
+# debug
+#from pudb import set_trace; set_trace()
 
-    max_ever = 0
+# leetcode 53
+# https://leetcode.com/problems/maximum-subarray/
+def maximum_subarray_53(nums:list) -> int:
+    if len(nums) == 0:
+        return 0
+
+    max_ever = nums[0]
     max_here = 0
 
     for elem in nums:
-        max_here = max_here + nums
+        max_here = max_here + elem
         # Ints may be positive or negative
+        if(max_here > max_ever):
+            max_ever = max_here
         if(max_here < 0):
             max_here = 0
-        if(max_ever < max_here):
-            max_ever = max_here
 
     return max_ever
+
+
+
+# leetcode 55
+# https://leetcode.com/problems/jump-game/
+# The key insight of this question is to note that a good jump is one
+# that allows you to land on position len(nums)-1. Therefore, if we are
+# walking towards the end (right hand side) of the array, we can only
+# terminate with a True (can reach) if the current position is at
+# len(nums) - 1. If we terminate elsewhere, we must return False
+def jump_game_55(nums:list) -> bool:
+
+    return jump_game_can_jump_basic(0, nums)
+
+
+# Inner function - determine if we can jump to the end from here
+def jump_game_can_jump_basic(cur_pos:int, nums:list) -> bool:
+    if cur_pos == len(nums)-1:
+        return True
+
+    if (cur_pos + nums[cur_pos]) > (len(nums)-1):
+        max_jump = (cur_pos + nums[cur_pos]) - len(nums)-1
+    else:
+        max_jump = nums[cur_pos]
+
+    for j in range(max_jump):
+        can_jump = jump_game_can_jump_basic(cur_pos + max_jump - j, nums)
+        if can_jump:
+            return True
+
+    return False
--- pylc_test/test_questions.py
+++ pylc_test/test_questions.py
@@ -9,11 +9,39 @@ import unittest
 from pylc import questions
 
 
-
 class TestQuestion53(unittest.TestCase):
     def setUp(self) -> None:
         self.inp1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-        self.exp1 = [4, -1, 2, 1]
+        self.exp1 = 6
+        self.inp2 = [-2]
+        self.exp2 = -2
+
+    def test_max_subarray(self) -> None:
+        s1 = questions.maximum_subarray_53(self.inp1)
+        self.assertEqual(self.exp1, s1)
+        s2 = questions.maximum_subarray_53(self.inp2)
+        self.assertEqual(self.exp2, s2)
+
+
+
+# For this question, we only need to know if we can or cannot make
+# it to the last index. We don't need to know the path.
+class TestQuestion55(unittest.TestCase):
+    def setUp(self) -> None:
+        self.inp1 = [2, 3, 1, 1, 4]
+        self.exp1 = True
+        self.inp2 = [3, 2, 1, 0, 4]
+        self.exp2 = False
+        self.inp3 = [1, 5, 2, 1, 0, 2, 0]
+        self.exp3 = True
+
+    def test_jump_game(self) -> None:
+        jump1 = questions.jump_game_55(self.inp1)
+        self.assertEqual(self.exp1, jump1)
+        jump2 = questions.jump_game_55(self.inp2)
+        self.assertEqual(self.exp2, jump2)
+        jump3 = questions.jump_game_55(self.inp3)
+        self.assertEqual(self.exp3, jump3)
 
 
 
