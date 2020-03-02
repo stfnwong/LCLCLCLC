4ae39817ad7aea02b44ef9de285971b9aa68d2f8
Stefan Wong <stfnwong@gmail.com>
2020-03-02 22:05:37 +0000

    Test cleanup

--- pylc_test/test_heap2.py
+++ pylc_test/test_heap2.py
@@ -44,7 +44,7 @@ class TestMaxHeap2(unittest.TestCase):
         self.assertEqual(True, heap2.has_max_heap_property(test_heap.heap))
         self.assertEqual(len(self.input_keys_1), len(test_heap))
 
-    def test_max_heap_remove(self) -> None:
+    def test_max_heap_remove_min(self) -> None:
         # NOTE: remember that this is a MaxHeap at the moment
         test_heap = heap2.MaxHeap()
         self.assertEqual([], test_heap.heap)
@@ -57,19 +57,24 @@ class TestMaxHeap2(unittest.TestCase):
             self.assertEqual(len(test_heap), n+1)
 
         print(test_heap)
-        print(test_heap.get_root())
         heap_min = test_heap.remove_min()
         self.assertEqual(1, heap_min)
+        self.assertEqual(True, heap2.has_max_heap_property(test_heap.heap))
 
         print(test_heap)
-        print(test_heap.get_root())
         heap_min = test_heap.remove_min()
         self.assertEqual(2, heap_min)
+        self.assertEqual(True, heap2.has_max_heap_property(test_heap.heap))
 
         print(test_heap)
-        print(test_heap.get_root())
         heap_min = test_heap.remove_min()
         self.assertEqual(3, heap_min)
+        self.assertEqual(True, heap2.has_max_heap_property(test_heap.heap))
+
+        print(test_heap)
+        heap_min = test_heap.remove_min()
+        self.assertEqual(7, heap_min)
+        self.assertEqual(True, heap2.has_max_heap_property(test_heap.heap))
 
 
 class TestMinHeap2(unittest.TestCase):
@@ -94,67 +99,6 @@ class TestMinHeap2(unittest.TestCase):
         self.assertEqual(True, heap2.has_min_heap_property(test_heap.heap))
         self.assertEqual(False, heap2.has_max_heap_property(test_heap.heap))
         self.assertEqual(len(self.input_keys_1), len(test_heap))
-
-
-
-#class TestHeap2(unittest.TestCase):
-#    def setUp(self) -> None:
-#        #self.input_keys            = [9, 8, 6, 7, 4, 5, 2, 0, 3, 1]
-#        #self.input_keys            = [5, 8, 3, 2, 4, 9, 7, 0, 6, 1]
-#        #self.input_keys            = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-#        #self.expected_keys         = [9, 8, 6, 7, 4, 5, 2, 0, 3, 1]
-#        #self.expected_keys_reverse = [9, 8, 7, 5, 6, 2, 1, 4, 0, 3]
-#        self.input_keys_1 = [1, 2, 3, 7, 17, 19, 25, 36, 100]
-#        self.input_keys_2 = [1, 2, 7, 3, 17, 36, 25, 19, 100]
-#
-#    def test_heap_2_init(self) -> None:
-#        # grab a heap
-#        # TODO : currently implemented as a MaxHeap
-#        test_heap = heap2.Heap()
-#        self.assertEqual([], test_heap.heap)
-#        self.assertEqual(0, len(test_heap))
-#
-#        # what happens as we add elements from self.input_keys?
-#        print('Adding keys %s to heap...' % str(self.input_keys_1))
-#        for n, k in enumerate(self.input_keys_1):
-#            test_heap.insert(k)
-#            self.assertEqual(len(test_heap), n+1)
-#
-#        print(test_heap)
-#        # Ensure that this is a min heap
-#        self.assertEqual(False, heap2.has_min_heap_property(test_heap.heap))
-#        self.assertEqual(True, heap2.has_max_heap_property(test_heap.heap))
-#        self.assertEqual(len(self.input_keys_1), len(test_heap))
-#
-#    def test_heap_2_remove(self) -> None:
-#        # NOTE: remember that this is a MaxHeap at the moment
-#        test_heap = heap2.Heap()
-#        self.assertEqual([], test_heap.heap)
-#        self.assertEqual(0, len(test_heap))
-#
-#        # what happens as we add elements from self.input_keys?
-#        print('Adding keys %s to heap...' % str(self.input_keys_1))
-#        for n, k in enumerate(self.input_keys_1):
-#            test_heap.insert(k)
-#            self.assertEqual(len(test_heap), n+1)
-#
-#        print(test_heap)
-#        print(test_heap.get_root())
-#        heap_min = test_heap.remove_min()
-#        self.assertEqual(1, heap_min)
-#
-#        print(test_heap)
-#        print(test_heap.get_root())
-#        heap_min = test_heap.remove_min()
-#        self.assertEqual(2, heap_min)
-#
-#        print(test_heap)
-#        print(test_heap.get_root())
-#        heap_min = test_heap.remove_min()
-#        self.assertEqual(3, heap_min)
-
-
-
 
 
 if __name__ == '__main__':
