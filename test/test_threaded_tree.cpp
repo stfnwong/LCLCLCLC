595cb9fe791b185c7f63f06f420409d1712fed20
Stefan Wong <stfnwong@gmail.com>
2020-03-22 10:49:29 +0000

    Moved threaded tree test over to catch2
    Note that this test doesn't really do anything at the moment anyway, but now it only needs catch.hpp

--- test/test_threaded_tree.cpp
+++ test/test_threaded_tree.cpp
@@ -5,44 +5,39 @@
  * Stefan Wong 2019
  */
 
+#define CATCH_CONFIG_MAIN
+#include "catch/catch.hpp"
 
 #include <vector>
 #include <string>
 #include <gtest/gtest.h>
 #include "threaded_tree.hpp"
 
+// test inputs 
+const std::string empty_repr = "[]";
+const std::string repr1      = "[1,2,3]";
+const std::string repr2      = "[1,null,2,3]";
+const std::string repr3      = "[1,null,2,3,null,4,null]";
+const int tree1_size = 3;
+const int tree2_size = 3;
+const int tree3_size = 4;
 
-class TestThreadedTree : public ::testing::Test
-{
-    virtual void SetUp() {}
-    virtual void TearDown() {}
 
-    public:
-        std::string empty_repr = "[]";
-        std::string repr1      = "[1,2,3]";
-        std::string repr2      = "[1,null,2,3]";
-        std::string repr3      = "[1,null,2,3,null,4,null]";
-
-        int tree1_size = 3;
-        int tree2_size = 3;
-        int tree3_size = 4;
-};
-
-TEST_F(TestThreadedTree, test_repr_to_threaded_tree)
+TEST_CASE("TestThreadedTree, test_repr_to_threaded_tree", "[classic]")
 {
     TreeNode* repr1_tree;
     TreeNode* repr2_tree;
     TreeNode* repr3_tree;
     TreeNode* empty_tree;
 
-    repr1_tree = repr_to_threaded_tree(this->repr1);
-    ASSERT_NE(nullptr, repr1_tree);
-    repr2_tree = repr_to_threaded_tree(this->repr2);
-    ASSERT_NE(nullptr, repr2_tree);
-    repr3_tree = repr_to_threaded_tree(this->repr3);
-    ASSERT_NE(nullptr, repr3_tree);
-    empty_tree = repr_to_threaded_tree(this->empty_repr);
-    ASSERT_EQ(nullptr, empty_tree);
+    repr1_tree = repr_to_threaded_tree(repr1);
+    REQUIRE(nullptr != repr1_tree);
+    repr2_tree = repr_to_threaded_tree(repr2);
+    REQUIRE(nullptr != repr2_tree);
+    repr3_tree = repr_to_threaded_tree(repr3);
+    REQUIRE(nullptr != repr3_tree);
+    empty_tree = repr_to_threaded_tree(empty_repr);
+    REQUIRE(nullptr == empty_tree);
     
     // How to test the actual content of the trees...?
 
@@ -51,12 +46,4 @@ TEST_F(TestThreadedTree, test_repr_to_threaded_tree)
     destroy_tree(repr2_tree);
     destroy_tree(repr3_tree);
     destroy_tree(empty_tree);
-}
-
-
-
-int main(int argc, char *argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
