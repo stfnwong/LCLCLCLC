67898436a12c2ca2fa002f9ef58f2ef97367a588
Stefan Wong <stfnwong@gmail.com>
2022-10-08 14:49:48 +0100

    move to catch

--- test/test_distance.cpp
+++ test/test_distance.cpp
@@ -4,55 +4,45 @@
  * Stefan Wong 2019
  */
 
+#define CATCH_CONFIG_MAIN
+#include "catch/catch.hpp"
+
 #include <vector>
-#include <gtest/gtest.h>
 #include "distance.hpp"
 
-class TestDistance : public ::testing::Test
-{
-    virtual void SetUp() {}
-    virtual void TearDown() {}
 
-    public:
-        std::vector<int> test_array = {3, 2, 1, 2, 1, 4, 5, 8, 6, 7, 4, 2};
-        int expected_dist_1 = 2;
-        int expected_dist_2 = 10;
-        int expected_dist_4 = 5;
-};
+std::vector<int> test_array = {3, 2, 1, 2, 1, 4, 5, 8, 6, 7, 4, 2};
+
+int expected_dist_1 = 2;
+int expected_dist_2 = 10;
+int expected_dist_4 = 5;
 
 
-TEST_F(TestDistance, test_occurrence_dist_brute)
+TEST_CASE("test_occurrence_dist_brute", "leetcode")
 {
     int dist_1;
     int dist_2;
     int dist_4;
 
-    dist_1 = occurrence_dist_brute(this->test_array, 1);
-    ASSERT_EQ(this->expected_dist_1, dist_1);
-    dist_2 = occurrence_dist_brute(this->test_array, 2);
-    ASSERT_EQ(this->expected_dist_2, dist_2);
-    dist_4 = occurrence_dist_brute(this->test_array, 4);
-    ASSERT_EQ(this->expected_dist_4, dist_4);
+    dist_1 = occurrence_dist_brute(test_array, 1);
+    REQUIRE(expected_dist_1 == dist_1);
+    dist_2 = occurrence_dist_brute(test_array, 2);
+    REQUIRE(expected_dist_2 == dist_2);
+    dist_4 = occurrence_dist_brute(test_array, 4);
+    REQUIRE(expected_dist_4 == dist_4);
 
 }
 
-TEST_F(TestDistance, test_occurrence_dist_hash)
+TEST_CASE("test_occurrence_dist_hash", "leetcode")
 {
     int dist_1;
     int dist_2;
     int dist_4;
 
-    dist_1 = occurrence_dist_hash(this->test_array, 1);
-    ASSERT_EQ(this->expected_dist_1, dist_1);
-    dist_2 = occurrence_dist_hash(this->test_array, 2);
-    ASSERT_EQ(this->expected_dist_2, dist_2);
-    dist_4 = occurrence_dist_hash(this->test_array, 4);
-    ASSERT_EQ(this->expected_dist_4, dist_4);
-}
-
-
-int main(int argc, char *argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    dist_1 = occurrence_dist_hash(test_array, 1);
+    REQUIRE(expected_dist_1 ==  dist_1);
+    dist_2 = occurrence_dist_hash(test_array, 2);
+    REQUIRE(expected_dist_2 ==  dist_2);
+    dist_4 = occurrence_dist_hash(test_array, 4);
+    REQUIRE(expected_dist_4 ==  dist_4);
 }
