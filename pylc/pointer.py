9dbb66a3cc6b3ecc1aaf1c0986a0de229426467e
Stefan Wong <stfnwong@gmail.com>
2022-10-19 01:31:04 +0100

    forgot to read +1 on index in question

--- pylc/pointer.py
+++ pylc/pointer.py
@@ -41,9 +41,32 @@ def two_sum_sort_1(nums: List[int], target:int) -> List[int]:
         s = val_idx[left][0] + val_idx[right][0]
         if s == target:
             return [val_idx[left][1], val_idx[right][1]]
-        elif s > target:    # too larget, reduce max value 
+        elif s > target:    # too large, reduce max value 
             right = right - 1
         else:   # too small, increase min value
             left = left + 1
 
     return []
+
+
+# This is an implementation of LC 167 - Two Sum II
+def two_sum_ii_2p_lc_167(nums: List[int], target: int) -> List[int]:
+    """
+    LC 167 In this problem the array is sorted.
+    """
+
+    # Since the array is sorted we can use the pointer at each end method directly
+    left = 0
+    right = len(nums) -1
+
+    while left < right:
+        s = nums[left] + nums[right]
+        if s == target:
+            return [left+1, right+1]
+        elif s > target:
+            right = right - 1
+        else:
+            left = left + 1
+
+    return []
+
--- pylc_test/test_pointers.py
+++ pylc_test/test_pointers.py
@@ -22,11 +22,45 @@ def test_two_sum():
         pointer.two_sum_sort_1
     ]
 
-    for func, target, inp, exp_out in zip(
-        functions,
-        targets,
-        inputs,
-        exp_outputs
-    ):
-        output = func(inp, target)
-        assert output == exp_out
+    for func in functions:
+        print(f"Testing function [{func.__name__}]")
+        for target, inp, exp_out in zip(
+            targets,
+            inputs,
+            exp_outputs
+        ):
+            output = func(inp, target)
+            assert output == exp_out
+
+
+
+def test_lc_167():
+
+    targets = [9, 6, -1]
+    inputs = [
+        [2, 7, 11, 15],
+        [2, 3, 4],
+        [-1, 0]
+    ]
+
+    exp_outputs = [
+        [1, 2],
+        [1, 3],
+        [1, 2]
+    ]
+
+    functions = [
+        pointer.two_sum_ii_2p_lc_167
+    ]
+
+    for func in functions:
+        print(f"Testing function [{func.__name__}]")
+        for target, inp, exp_out in zip(
+            targets,
+            inputs,
+            exp_outputs
+        ):
+            print(f"inp:  {inp}")
+            output = func(inp, target)
+            assert output == exp_out
+
