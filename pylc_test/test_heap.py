1c8cfa2889555f024273b3a3035e9b1b9f7d1998
Stefan Wong <stfnwong@gmail.com>
2022-11-06 22:10:15 +0000

    more cleanup

--- pylc_test/test_heap.py
+++ pylc_test/test_heap.py
@@ -4,9 +4,6 @@ Heap2 unit tests. This is just a heap with less stuff attached to it
 
 """
 
-import random
-import copy
-import unittest
 # unit under test
 from pylc import heap      # Once this is alright, factor back into main heap
 
@@ -19,11 +16,9 @@ def test_max_heap_init():
     test_heap = heap.MaxHeap()
     assert len(test_heap) == 0
 
-
 def test_max_heap_init_from_list():
     test_heap = heap.MaxHeap(heap_inp_array_1)
     assert heap.has_max_heap_property(test_heap.heap)
-
 
 def test_create_max_heap_by_insertion():
     test_heap = heap.MaxHeap()
