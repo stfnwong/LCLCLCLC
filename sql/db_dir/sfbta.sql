11f45a20576962c5540f41cf497b95eefa8684c7
Stefan Wong <stfnwong@gmail.com>
2023-01-24 01:19:53 +0000

    more stuff

--- sql/db_dir/sfbta.sql
+++ sql/db_dir/sfbta.sql
@@ -7,7 +7,6 @@ DROP DATABASE actors_db;
 CREATE DATABASE actors_db;
 \connect actors_db;
 
--- I understand that SERIAL is the Postgres equivalent of AUTO_INCREMENT on MySQL
 -- In Postgres it looks like we don't have the ENUM keyword. Instead we create a new 
 -- type using the CREATE TYPE name AS ENUM and then use that type in the CREATE TABLE
 -- section.
@@ -16,6 +15,7 @@ CREATE TYPE GENDER AS ENUM ('Male', 'Female', 'Other');
 CREATE TYPE MARITAL_STATUS AS ENUM ('Married', 'Divorced', 'Single', 'Unknown');
 
 CREATE TABLE IF NOT EXISTS actors (
+    -- The SERIAL type is the equivalent of INT AUTO_INCREMENT in MySQL
     id SERIAL,
     first_name VARCHAR(20) NOT NULL,
     second_name VARCHAR(20) NOT NULL,
@@ -42,3 +42,38 @@ VALUES ('Jennifer', 'Aniston', '1969-11-02', 'Female', 'Single', 240.00),
 -- We don't need to list all the column names if the set of values is complete. 
 INSERT INTO actors 
 VALUES (DEFAULT, 'Dream', 'Actress', '9999-01-01', 'Female', 'Single', 000.00);
+
+
+-- Display again
+\d actors;
+
+-- Query the table 
+\echo 'SELECT * FROM actors'
+SELECT * FROM actors;
+
+SELECT first_name, second_name FROM actors;
+
+SELECT first_name, second_name FROM actors WHERE first_name = 'Travolta'; -- expect no output
+SELECT first_name. second_name FROM actors WHERE first_name = 'Brad';
+SELECT first_name, second_name FROM actors WHERE net_worth_millions > 500; -- no actor is worth this much...
+SELECT first_name, second_name FROM actors WHERE net_worth_millions > 1;
+
+
+-- LIKE operator 
+\echo '[SELECT * FROM actors WHERE first_name LIKE Jen%]'
+SELECT * FROM actors WHERE first_name LIKE 'Jen%';      -- '%' is wildcard operator
+\echo '[SELECT * FROM actors WHERE first_name LIKE %]'
+SELECT * FROM actors WHERE first_name LIKE '%';
+\echo '[SELECT first_name, second_name FROM actors WHERE first_name LIKE _enn%]'
+SELECT first_name, second_name FROM actors WHERE first_name LIKE '_enn%';
+
+
+-- Can we show databases in postgres this way?
+-- SHOW DATABASES LIKE 'a%';
+-- But we can query the pg_database catalog for a database name 
+SELECT datname FROM pg_database;
+
+-- This (below) doesn't work
+-- SELECT datname FROM pg_database LIKE '%tors';
+-- But this (below) does
+SELECT datname FROM pg_database WHERE datname LIKE '%tors';
